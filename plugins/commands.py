1e75ec052460dd4d123e229e0f3a424571fcc8e5
bissness11 <154408458+bissness11@users.noreply.github.com>
2024-08-17 21:35:25 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
-from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.errors import ChatAdminRequired, FloodWait,UserNotParticipant
 from pyrogram.types import *
 from utils import verify_user, check_token, check_verification, get_token
 from config import *
