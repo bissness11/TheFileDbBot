ecf71fa54d2c1d0b1c122c515392fe4158cdcb2b
bissness11 <154408458+bissness11@users.noreply.github.com>
2024-08-18 16:15:11 +0500

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -16,7 +16,7 @@ import json
 import base64
 import logging
 from plugins import commands
-from commands import *
+#from commands import *
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
